fa590dbbe9766d4e6c930e2b3c8f1274627f82c6
omarbenjdidia <92368449+omarbenjdidia@users.noreply.github.com>
2023-10-12 15:17:03 +0100

    Update UserServiceImplTest.java

--- src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
+++ src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
@@ -1,10 +1,26 @@
-package tn.esprit.spring.services;
-
-
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit jupiter.api-Assertions;
+import org.junit.jupiter.api. MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit jupiter.api.lest;
+import org.junit jupiter.api.Test MethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
 
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
-class UserServiceImplTest {}
+class UserServicelmplTest {
+
+@Autowired
+IUserService us;
+
+@Test
+@Order(1)
+public void testRetrieveAllUsers() {
+List<User> listUsers = us.retrieveAllUsers();
+Assertions.assertEquals(0, listUsers.size());
+
+}
+
+ }
